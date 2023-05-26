5e3bf5beb12c226f397ae03756ee2cac9dcf8f72
Jean-f-05 <jean_f_05@hotmail.com>
2023-05-26 21:06:01 +0100

    add: Menu (to Navbar) compontent & styles

---
+++ src/app/components/Menu/index.tsx
@@ -0,0 +1,46 @@
+import styles from "./styles.module.css";
+import { motion } from "framer-motion";
+
+type MenuProps = {
+    isOpen: boolean,
+    setMenu: (value: boolean) => void,
+
+}
+
+const Menu = ({ isOpen, setMenu }: MenuProps) => {
+
+    const variants = {
+        open: { opacity: 1, x: 0, width: '100%' },
+        closed: { opacity: 0, x: "-100%", width: 0 },
+    }
+
+    return (
+        <>
+            <motion.div
+                className={styles.menu__wrapper}
+                animate={isOpen ? "open" : "closed"}
+                variants={variants}
+            >
+                <ul className={styles.menu__ulist}>
+                    <li>
+                        <a href="#whoami" className={styles.menu__anchor} onClick={() => setMenu(false)}>
+                            Who am I <span>?</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#stack" className={styles.menu__anchor} onClick={() => setMenu(false)}>
+                            The stack <span>...</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#portfolio" className={styles.menu__anchor} onClick={() => setMenu(false)}>
+                            My Portfolio <span>!</span>
+                        </a>
+                    </li>
+                </ul>
+            </motion.div>
+        </>
+    )
+}
+
+export default Menu
