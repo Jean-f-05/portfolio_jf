471a1f7aa5eafe0cc89a016c190f5d04aa8dbcd0
Jean-f-05 <jean_f_05@hotmail.com>
2023-05-28 15:38:38 +0100

    add stack mock data

---
+++ src/app/mocks/stackmock.ts
@@ -0,0 +1,58 @@
+import {  FaHtml5, FaCss3Alt, FaNodeJs } from 'react-icons/fa';
+import {  TbBrandNextjs } from 'react-icons/tb';
+import { SiMysql, SiStyledcomponents, SiJest, SiJavascript, SiTypescript, SiReact } from 'react-icons/si'
+
+const icons = [
+    {
+        Icon:SiReact,
+        size:85,
+        color: '#ADD8E6'
+    }, 
+    {
+        Icon:FaHtml5,
+        size:85,
+        color:'#FF5733'
+    },
+    {
+        Icon:FaCss3Alt,
+        size:85,
+        color: '#6082B6'
+    },
+    {
+        Icon:FaNodeJs,
+        size:85,
+        color:'#478778'
+    },
+    {
+        Icon:SiTypescript,
+        size:85,
+        color: '#6082B6'
+    },
+    {
+        Icon:SiJavascript,
+        size:85,
+        color:'#FFD700'
+    },
+    {
+        Icon:TbBrandNextjs,
+        size:85,
+        color:'#353935'
+    },
+    {
+        Icon:SiMysql,
+        size:85,
+        color:'#6F8FAF'
+    },
+    {
+        Icon:SiStyledcomponents,
+        size:85,
+        color:'#353935'
+    },
+    {
+        Icon:SiJest,
+        size:85,
+        color:'#A95C68'
+    }
+]
+
+export default icons
