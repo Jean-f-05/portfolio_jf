c76c9567044119ed423fde4d03eac7a259e51c70
Jean-f-05 <jean_f_05@hotmail.com>
2023-05-29 10:19:14 +0100

    add: icon id to mock data

--- src/app/mocks/stackmock.ts
+++ src/app/mocks/stackmock.ts
@@ -6,52 +6,62 @@ const icons = [
     {
         Icon:SiReact,
         size:85,
-        color: '#ADD8E6'
+        color: '#ADD8E6',
+        id:"React"
     }, 
     {
         Icon:FaHtml5,
         size:85,
-        color:'#FF5733'
+        color:'#FF5733',
+        id:"Html"
     },
     {
         Icon:FaCss3Alt,
         size:85,
-        color: '#6082B6'
+        color: '#6082B6',
+        id:"Css"
     },
     {
         Icon:FaNodeJs,
         size:85,
-        color:'#478778'
+        color:'#478778',
+        id:"Node"
     },
     {
         Icon:SiTypescript,
         size:85,
-        color: '#6082B6'
+        color: '#6082B6',
+        id:"Typescript"
     },
     {
         Icon:SiJavascript,
         size:85,
-        color:'#FFD700'
+        color:'#FFD700',
+        id:"Javascript"
     },
     {
         Icon:TbBrandNextjs,
         size:85,
-        color:'#353935'
+        color:'#353935',
+        id:"Next"
     },
     {
         Icon:SiMysql,
         size:85,
-        color:'#6F8FAF'
+        color:'#6F8FAF',
+        id:"Mysql"
     },
     {
         Icon:SiStyledcomponents,
         size:85,
-        color:'#353935'
+        color:'#353935',
+        id:"Styledcomponents"
     },
     {
         Icon:SiJest,
         size:85,
-        color:'#A95C68'
+        color:'#A95C68',
+        id:"jest"
     }
 ]
 
