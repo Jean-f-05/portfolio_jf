17cf723683fcef832f240f21f46229b63078173f
Jean-f-05 <jean_f_05@hotmail.com>
2023-05-30 18:16:51 +0100

    add: fetched data to Navbar

--- src/app/components/Navbar/index.tsx
+++ src/app/components/Navbar/index.tsx
@@ -4,13 +4,17 @@ import { useState } from 'react';
 import { RiMenuUnfoldLine, RiCloseFill } from 'react-icons/ri';
 import styles from "./styles.module.css";
 
-const Navbar = () => {
+type NavBarProps = {
+    title: string
+}
+
+const Navbar = ({ title }: NavBarProps) => {
     const [menuOpen, setMenuOpen] = useState(false);
 
     return (<>
         <nav className={styles.navbar}>
             <ul className={styles.navbar__ul}>
-                <a href={""} className={styles.navbar__a}>Jean&apos;s Portfolio</a>
+                <a href={""} className={styles.navbar__a}>{title}</a>
                 <div>
                     {menuOpen ? <RiCloseFill size={32} color='#F7EDE7' onClick={() => { setMenuOpen(!menuOpen) }} /> : <RiMenuUnfoldLine size={32} color='#F7EDE7' onClick={() => { setMenuOpen(!menuOpen) }} />}
                 </div>
