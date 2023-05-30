ddf45baad7e0f9a8424a1b06d707e85b9f7fd038
Jean-f-05 <jean_f_05@hotmail.com>
2023-05-30 20:29:21 +0100

    add: fetched data to ProjectElement

--- src/app/components/ProjectElement/index.tsx
+++ src/app/components/ProjectElement/index.tsx
@@ -4,17 +4,17 @@ import { SiGithub } from 'react-icons/si';
 import Link from 'next/link';
 
 type ProjectProps = {
-    type: "Web App" | "Landing Page",
-    name: string,
-    description: string,
-    linkRef: string,
-    stickers: Array<"NextJS" | "React" | "Styled Components" | "Html5" | "Css3" | "Typescript">
-    image: ImageProps
+    cardtype: "Web App" | "Landing Page",
+    cardName: string,
+    cardDescription: string,
+    cardLink: string,
+    cardStickers: Array<"NextJS" | "React" | "Styled Components" | "Html5" | "Css3" | "Typescript">
+    cardImage: ImageProps
 }
 
 type ImageProps = {
     alt: string,
-    url: string
+    image: any
 }
 
 type StickerProps = {
@@ -28,31 +28,36 @@ const Sticker = ({ children }: StickerProps) => {
     )
 }
 
-
-const ProjectElement = ({ type, name, description, linkRef, stickers, image }: ProjectProps) => {
+const ProjectElement = ({ data }: any) => {
     return (
-        <div className={styles.project__wrapper}>
-            <div className={styles.project__image__wrapper}>
-                <Image alt={image.alt} src={image.url} fill />
-            </div>
-            <div className={styles.project__header}>
-                <h5 className={styles.project__text}>{type}</h5>
-                <Link href={linkRef} target="_blank">
-                    <SiGithub color='#fff' size={28} />
-                </Link>
-            </div>
-            <strong className={styles.project__name}>
-                {name}
-            </strong>
-            <p className={styles.project__paragraph}>
-                {description}
-            </p>
-            <div className={styles.stickers__wrapper}>
-                {stickers.map((sticker) => {
-                    return <Sticker key={sticker}>{sticker}</Sticker>
-                })}
-            </div>
-        </div>)
+        data.map((value: ProjectProps) => {
+            const { cardImage, cardtype, cardName, cardDescription, cardLink, cardStickers } = value;
+            return (
+                < div className={styles.project__wrapper} key={cardName}>
+                    <div className={styles.project__image__wrapper}>
+                        <Image alt={cardImage.alt} src={cardImage.image.url} fill />
+                    </div>
+                    <div className={styles.project__header}>
+                        <h5 className={styles.project__text}>{cardtype}</h5>
+                        <Link href={cardLink} target="_blank">
+                            <SiGithub color='#fff' size={28} />
+                        </Link>
+                    </div>
+                    <strong className={styles.project__name}>
+                        {cardName}
+                    </strong>
+                    <p className={styles.project__paragraph}>
+                        {cardDescription}
+                    </p>
+                    <div className={styles.stickers__wrapper}>
+                        {cardStickers.map((sticker: any) => {
+                            return <Sticker key={sticker}>{sticker}</Sticker>
+                        })}
+                    </div>
+                </div >
+            )
+        })
+    )
 }
 
 export default ProjectElement
