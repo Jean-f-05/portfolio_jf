9108ddbf0152b466374a4b593221fd3402398d6d
Jean-f-05 <jean_f_05@hotmail.com>
2023-05-30 11:30:40 +0100

    up: whoami mock now includes text

--- src/app/mocks/mockwhoami.ts
+++ src/app/mocks/mockwhoami.ts
@@ -1,38 +1,47 @@
+const height = 250
+const width = 250
+const style = "thumb__image"
+
 const whoami = [
     {
-        height:230, 
-        width:230,
+        height:height, 
+        width:width,
         alt: "a watercolor painting of a dog with a red bandana near the shore",
         src:"/dogo.webp",
-        style:"thumb__image"
+        style:style,
+        text:"Dogs, I absolutely love them." 
     },
     {
-        height:230, 
-        width:230,
+        height:height, 
+        width:width,
         alt: "a watercolor painting of a coder working sitting at his desk",
         src:"/desk.webp",
-        style:"thumb__image"
+        style:style,
+        text: "Coding 'This is the way!'"
     },
     {
-        height:230, 
-        width:230,
+        height:height, 
+        width:width,
         alt: 'a watercolor painting of a gamer playing a videogame with enthusiasm',
         src:"/gamer.webp",
-        style:"thumb__image"
+        style:style,
+        text: "ocasional gamer. Happy owner of a switch"
     },
     {
-        height:230, 
-        width:230,
+        height:height, 
+        width:width,
         alt: 'a watercolor painting of a basketball player aiming at the hoop',
         src:"/basket.webp",
-        style:"thumb__image"
+        style:style,
+        text: "Basketball player since I was 10."
     },
     {
-        height:230, 
-        width:230,
+        height:height, 
+        width:width,
         alt: 'a watercolor painting of a book and a hot coffee',
         src:"/bookandcoffee.webp",
-        style:"thumb__image"
+        style:style,
+        text: "Coffee. i honestly have no idea how people lived before it."
     },
 ]
 
