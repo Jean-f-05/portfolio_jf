0a52d846079ed36fa40bf705d274997a5cbe706c
Jean-f-05 <jean_f_05@hotmail.com>
2023-05-31 11:32:15 +0100

    refactor: footer query fetches elements array

--- src/app/getData/queries.ts
+++ src/app/getData/queries.ts
@@ -1,84 +1,83 @@
 export const GET_HOME = `
 query MyQuery {
-    homes {
-      title
-      heroImage {
+  homes {
+    title
+    heroImage {
+      alt
+      image {
+        url
+      }
+    }
+    heroText {
+      firstLine
+      secondLine
+      thirdLine
+      fourthLine
+    }
+    whoamiSection {
+      sectionRef
+      text
+      sectionBg {
+        url
+      }
+    }
+    whoamiParagraph
+    carousel {
+      ... on Carousel {
+        id
+        carouselImage {
+          ... on Image {
+            id
+            alt
+            text
+            image {
+              id
+              url
+            }
+          }
+        }
+      }
+    }
+    stackSection {
+      sectionRef
+      text
+      sectionColor
+    }
+    portfolioSection {
+      sectionRef
+      text
+      sectionColor
+      sectionBg {
+        url
+      }
+    }
+    portfolioCard {
+      cardImage {
         alt
         image {
           url
         }
       }
-      heroText {
-        firstLine
-        secondLine
-        thirdLine
-        fourthLine
-      }
-      whoamiSection {
-        sectionRef
-        text
-        sectionBg {
-          url
-        }
-      }
-      whoamiParagraph
-      carousel {
-        ... on Carousel {
-          id
-          carouselImage {
-            ... on Image {
-              id
-              alt
-              text
-              image {
-                id
-                url
-              }
-            }
-          }
-        }
-      }
-      stackSection {
-        sectionRef
-        text
-        sectionColor
-      }
-      portfolioSection {
-        sectionRef
-        text
-        sectionColor
-        sectionBg {
-          url
-        }
-      }
-      portfolioCard {
-        cardImage {
-          alt
-          image {
-            url
-          }
-        }
-        cardType
-        cardName
-        cardDescription
-        cardLink
-        cardStickers
-      }
+      cardType
+      cardName
+      cardDescription
+      cardLink
+      cardStickers
+    }
+    footer {
       footerSection {
         sectionRef
         text
       }
-      footerElement {
-        footerIcon {
-          iconName
-          iconColor {
-            hex
-          }
-          iconSize
-        }
+      elements {
+        icon
+        text
+        href
+      }
+      copyright {
         text
       }
     }
   }
-  
+}
 `  
