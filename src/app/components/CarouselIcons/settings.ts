ca91599caa370b06db3db1a88572919e274f6f13
Jean-f-05 <jean_f_05@hotmail.com>
2023-06-02 13:15:58 +0100

    minor CarouselIcon settings fix

--- src/app/components/CarouselIcons/settings.ts
+++ src/app/components/CarouselIcons/settings.ts
@@ -10,33 +10,49 @@ const settings = {
       {
         breakpoint: 2048,
         settings: {
-          slidesToShow: 4,
-          slidesToScroll: 4,
           infinite: true,
+          slidesToShow: 7,
+          slidesToScroll: 1,
+          autoplay: true,
+          speed: 2000,
+          autoplaySpeed: 2000,
+          cssEase: "linear",
         }
       },
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
           infinite: true,
+          slidesToShow: 5,
+          slidesToScroll: 1,
+          autoplay: true,
+          speed: 2000,
+          autoplaySpeed: 2000,
+          cssEase: "linear",
         }
       },
       {
-        breakpoint: 600,
+        breakpoint: 800,
         settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
           infinite: true,
-
+          slidesToShow: 4,
+          slidesToScroll: 1,
+          autoplay: true,
+          speed: 2000,
+          autoplaySpeed: 2000,
+          cssEase: "linear",
         }
       },
       {
-        breakpoint: 400,
+        breakpoint: 450,
         settings: {
+          infinite: true,
           slidesToShow: 3,
           slidesToScroll: 1,
+          autoplay: true,
+          speed: 2000,
+          autoplaySpeed: 2000,
+          cssEase: "linear",
         }
       }
     ]
