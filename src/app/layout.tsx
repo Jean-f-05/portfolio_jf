b69c749402150e095ee34ae92783800816af8f17
Jean-f-05 <jean_f_05@hotmail.com>
2023-06-03 16:54:05 +0100

    add: vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Koulen } from 'next/font/google'
+import { Analytics } from '@vercel/analytics/react';
 
 const koulen = Koulen({
   weight: '400',
@@ -18,6 +19,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Analytics />
       <body className={koulen.className}>{children}</body>
     </html>
   )
